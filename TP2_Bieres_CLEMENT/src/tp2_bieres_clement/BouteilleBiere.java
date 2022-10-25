5fd71f42a50dc51b5ea4ae7f7d7722d6688c5ce7
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-10-25 16:45:15 +0200

    fin du projet

--- TP2_Bieres_CLEMENT/src/tp2_bieres_clement/BouteilleBiere.java
+++ TP2_Bieres_CLEMENT/src/tp2_bieres_clement/BouteilleBiere.java
@@ -16,16 +16,32 @@ public class BouteilleBiere {
     
     public void lireEtiquette() {
 System.out.println("Bouteille de " + nom +" (" + degreAlcool +
-" degres) \nBrasserie : " + brasserie ) ;
+" degres) \nBrasserie : " + brasserie + ouverte ) ;
     }
-public BouteilleBiere(String unNom, float unDegre, String
+public BouteilleBiere(String unNom, double unDegre, String
 uneBrasserie) {
 nom = unNom;
 degreAlcool = unDegre;
 brasserie = uneBrasserie;
 ouverte = false;
-
-}   
 }
+ public void Décapsuler(){
+    if (ouverte == false){
+        ouverte = true;
+    } else{
+        System.out.println("erreur : biere déjà ouverte");
+        ouverte = false;
+    }
+ }
+ @Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
+}   
+
 
 
