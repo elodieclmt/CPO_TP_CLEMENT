03a83a1778779b9457a1126b776570cfa9d7d78c
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-10-25 15:17:13 +0200

    création d'une classe

---
+++ TP2_Bieres_CLEMENT/src/tp2_bieres_clement/TP2_Bieres_CLEMENT.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_clement;
+
+/**
+ *
+ * @author Elodie
+ */
+public class TP2_Bieres_CLEMENT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      BouteilleBiere uneBiere = new BouteilleBiere() ;
+   uneBiere.nom = "Cuvée des trolls";
+   uneBiere.degreAlcool = 7.0 ;
+   uneBiere.brasserie = "Dubuisson" ;  
+   uneBiere.ouverte = false;
+   
+    }
+    
+
+}
