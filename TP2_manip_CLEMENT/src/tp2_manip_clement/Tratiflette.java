3cf059b6a8edadbf9eaa7e1738dab0a2d67a6a31
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-10-28 16:02:40 +0200

    Début projet création des classes

--- TP2_manip_CLEMENT/src/tp2_manip_clement/Tratiflette.java
+++ TP2_manip_CLEMENT/src/tp2_manip_clement/Tratiflette.java
@@ -1,14 +1,14 @@
+package tp2_manip_clement;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tp2_manip_clement;
 
 /**
  *
  * @author Elodie
  */
-public class Moussaka {
- 
+public class Tratiflette {
     
 }
