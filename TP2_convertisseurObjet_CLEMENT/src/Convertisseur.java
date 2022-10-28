6891bf3edba902a4fc4d0d81c6c4ddca83a8d930
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-10-28 14:20:28 +0200

    Création des méthodes

--- TP2_convertisseurObjet_CLEMENT/src/Convertisseur.java
+++ TP2_convertisseurObjet_CLEMENT/src/Convertisseur.java
@@ -8,5 +8,39 @@
  * @author Elodie
  */
 public class Convertisseur {
+ 
+    int nbConversions;
     
+    public Convertisseur () {
+nbConversions = 0 ;
+nbConversions += 1;
+}
+    public void CelciusVersKelvin(double CVK){
+        CVK = CVK + 273.15;    
+    }
+    
+    public void KelvinVersClescius (double KVC){
+        KVC = KVC - 273.15;    
+    }
+    
+    public void FarenheitVersCelcius (double FVC){
+        FVC = (FVC - 32)*(5/9);    
+    }
+    
+    public void CelciusVersFarenheit (double CVF){
+        CVF = (CVF*(9/5))+32;    
+    }
+    
+        public void FarenheitVersKelvin (double FVK){
+        FVK = (FVK-32)*(5/9)+273.15 ;    
+    }
+        
+    public void KelvinVersFarenheit (double KVF){
+        KVF = (KVF -273.15)*(9/5)+32;    
+    }   
+     
+    @Override
+public String toString () {
+return "nb de conversions"+ nbConversions;
+}
 }
