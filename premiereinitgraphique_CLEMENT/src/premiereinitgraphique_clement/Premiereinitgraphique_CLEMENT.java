41f79f25db66e8a635327e858f75f169c6fde060
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-11-07 12:26:28 +0100

    Fin du tp suivi

--- premiereinitgraphique_CLEMENT/src/premiereinitgraphique_clement/Premiereinitgraphique_CLEMENT.java
+++ premiereinitgraphique_CLEMENT/src/premiereinitgraphique_clement/Premiereinitgraphique_CLEMENT.java
@@ -13,8 +13,6 @@ public class Premiereinitgraphique_CLEMENT {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    
     
 }
