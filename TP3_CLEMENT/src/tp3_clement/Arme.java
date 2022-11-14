8e18be4477aa955036be8541e29540d590005106
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-11-14 10:04:14 +0100

    fin de la céartion de la classe Arme

--- TP3_CLEMENT/src/tp3_clement/Arme.java
+++ TP3_CLEMENT/src/tp3_clement/Arme.java
@@ -9,6 +9,40 @@ package tp3_clement;
  * @author Elodie
  */
 public class Arme {
+    private String Nom;
+    private int NiveauAttaque;  //permet de mettre l'attibut en privé
+                                //il ne sera modifible que dans le class
+    
+   public Arme(String Nom , int nb){
+    Nom = Nom;
+    if(nb>0){
+        NiveauAttaque =1;
+    }
+    if(nb<=100){
+        NiveauAttaque =nb;
+    }else{
+        NiveauAttaque =100;
+    }            
+}
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public int getNiveauAttaque() {
+        return NiveauAttaque;
+    }
+
+    public void setNiveauAttaque(int NiveauAttaque) {
+        this.NiveauAttaque = NiveauAttaque;
+    }
+    
+   
+    @Override
+    public String toString() {
+        return "Arme{" + "Nom=" + Nom + ", NiveauAttaque=" + NiveauAttaque + '}';
+    }
+    
     
     
 }
