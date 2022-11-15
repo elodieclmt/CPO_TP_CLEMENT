ae6046bb54527eb9646fdd4b84d2b31e156d9920
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-11-15 14:14:11 +0100

    Créations du tableau arryalist

--- TP3_CLEMENT/src/tp3_clement/TP3_CLEMENT.java
+++ TP3_CLEMENT/src/tp3_clement/TP3_CLEMENT.java
@@ -4,6 +4,8 @@
  */
 package tp3_clement;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Elodie
@@ -16,11 +18,19 @@ public class TP3_CLEMENT {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        //création de 4 objet (2 épées et 2 batons)
         Epee exp = new Epee(5, "Excalibur",7);
         Epee dur = new Epee(7, "Durandal", 4);
         
         Baton chene = new Baton("Chêne", 4, 5);
         Baton charme = new Baton("Charme", 5, 6);
+        
+        ArrayList<Arme> Tab = new ArrayList<Arme>(); //création tableau dynamique
+        Tab.add(exp);
+        Tab.add(dur);
+        Tab.add(chene);
+        Tab.add(charme);
+        
     }
     
 }
