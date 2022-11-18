0c3d53822bb5e4c4bd0fbe711db27a38644830d5
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-11-18 16:16:40 +0100

    Fin de la création de la classe CelluleDeGrille ( version 1)

---
+++ SP4_console_BRAS_CLEMENT/src/sp4_console_bras_clement/CelluleDeGrille.java
@@ -0,0 +1,48 @@
+package sp4_console_bras_clement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Elodie
+ */
+public class CelluleDeGrille {
+  
+    private Jeton jetonCourant;
+
+    public CelluleDeGrille() {
+        jetonCourant=null;
+    }
+
+     
+    public boolean presenceJeton(){
+        if(jetonCourant==null){
+            return false;
+            }
+        else{
+            return true;
+        }
+    }
+
+    public void AffecterJeton(Jeton jetonCourant) {
+        this.jetonCourant = jetonCourant;
+    }
+    
+   public String lireCouleurDuJeton(){
+       if(jetonCourant != null){
+           return jetonCourant.lireCouleur();
+       }else{
+           return "vide";
+       }
+   }
+   
+  public Jeton recupererJeton(){
+      Jeton tmp = jetonCourant;
+      jetonCourant=null;
+      return tmp;
+  } 
+    
+}
