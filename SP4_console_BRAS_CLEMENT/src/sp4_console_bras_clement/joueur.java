30cafa82f1445010bda4e81a9373667db1d3658d
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-11-28 18:42:02 +0100

    fin création de la classe joueur( version 1)

--- SP4_console_BRAS_CLEMENT/src/sp4_console_bras_clement/joueur.java
+++ SP4_console_BRAS_CLEMENT/src/sp4_console_bras_clement/joueur.java
@@ -4,10 +4,36 @@
  */
 package sp4_console_bras_clement;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Elodie
  */
 public class joueur {
     
+    private String nom;
+    private String couleur;
+    private ArrayList<Jeton> reserveJeton = new ArrayList<Jeton>();
+
+    public joueur(String nom) {
+        this.nom = nom;
+    }
+    
+    public void affecterCouleur(String couleur){
+        this.couleur = couleur;
+    }
+    
+    public int nombreDeJeton(){
+        return reserveJeton.size();
+    }
+    
+    public void ajouterJeton(Jeton ajoutJeton){
+        reserveJeton.add(ajoutJeton);
+    }
+    
+    public Jeton jouerJeton(){
+        Jeton j=reserveJeton.remove(0);
+        return j;
+    }
 }
