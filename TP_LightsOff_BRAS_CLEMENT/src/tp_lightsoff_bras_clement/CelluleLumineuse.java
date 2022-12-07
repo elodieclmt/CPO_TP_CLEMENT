db5bc2a31e58cfe1cc7abba98f79996d1cb7f741
Elodie <Elodie@LAPTOP-VNUO60S6>
2022-12-07 10:25:36 +0100

    Création de la classe grille

--- TP_LightsOff_BRAS_CLEMENT/src/tp_lightsoff_bras_clement/CelluleLumineuse.java
+++ TP_LightsOff_BRAS_CLEMENT/src/tp_lightsoff_bras_clement/CelluleLumineuse.java
@@ -11,4 +11,69 @@ package tp_lightsoff_bras_clement;
  */
 public class CelluleLumineuse {
     
+    boolean etat;
+
+    /**
+     * Initialisation de la valeur de la cellule (allumée ou éteinte)
+     * @param etat
+     */
+    public CelluleLumineuse(boolean etat) {
+        this.etat = etat;
+    }
+
+    /**
+     * retourne l'état (vrai pour allumé et faux pour éteint)
+     * @return
+     */
+    public boolean LireEtat() {
+        return etat;
+    }
+    
+    /**
+     * donne la valeur vrai (allumé) à la case
+     */
+    public void AllumeToi(){
+        etat=true;
+    }
+    
+    /**
+     *donne la valeur faux (éteint) à la case
+     */
+    public void EteintToi(){
+        etat=false;
+    }
+    
+    /**
+     * change la valeur de la case
+     * passe les cellules allumées à éteintes et inversement
+     * @param valeur
+     */
+    public void ChangeTonEtat(boolean valeur){
+        if (valeur == true){
+            valeur = false;
+        }
+        else{
+            valeur = true;
+        } 
+    }
+
+    /**
+     * affiche l'etat de la cellule
+     * @return
+     */
+    @Override
+    public String toString() {
+        if (etat == true){
+            return "A";
+        }
+        else{
+            return "E";
+        }
+        
+    }
+    
+    
+    
+    
+    
 }
